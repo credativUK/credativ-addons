5474f489d259c36653f91bb7e5eeaaa618f9a601
Tristan Hill <tristan.hill@credativ.co.uk>
2012-05-02 12:41:41 +0100

    [IMP] module to run procurement scheduler on sale order confirm

---
+++ sale_run_procurement_scheduler/sale.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 credativ ltd (<http://www.credativ.co.uk>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+
+class sale_order(osv.osv):
+    _inherit = "sale.order"
+
+    def action_ship_create(self, cr, uid, ids, *args):
+        res = super(sale_order, self).action_ship_create(cr, uid, ids, *args)
+        all_pool = self.pool.get('procurement.order.compute.all')
+        id_ = all_pool.create(cr, uid, {})
+        all_pool.procure_calculation(cr, uid, [id_], {})
+        all_pool.unlink(cr, uid, [id_]) # TODO: confirm this is needed?
+        return res
+
+sale_order()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
