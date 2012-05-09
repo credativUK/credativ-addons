5301b0034724c2491e329a3b2949329f02b35e1a
Craig Gowing (credativ) <craig.gowing@credativ.co.uk>
2012-05-09 12:48:26 +0100

    [ADD] Backport action_ship_create sub routines from 6.1 to 5.0 for easy modification

---
+++ sale_single_pickings/__terp__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Sale Single Pickings',
+    'version': '1.0',
+    'category': 'Generic Modules/Sales & Purchases',
+    'description': """This Module backports Sale Order action_ship_create from v6.1 and creates single pickings and stock moves per single item.""",
+    'author': 'Credativ',
+    'depends': ['sale'],
+    'init_xml': [],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+    'certificate': '',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
