2481b784ceeb516bbe0582cb6bea187429cbf269
Craig Gowing (credativ) <craig.gowing@credativ.co.uk>
2012-05-17 16:36:19 +0100

    [ADD] Add module country_zipregion which allows searching through Partners, Addresses, Sale Orders, Stock Moves and Stock Pickings by a group of Countires and Zip Code RegEx strings

---
+++ country_zipregion/__terp__.py
@@ -0,0 +1,51 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name' : 'Zip Region',
+    'version' : '1.0.0.0',
+    'author' : 'credativ',
+    'website' : 'http://credativ.co.uk',
+    'depends' : [
+        'base',
+        'stock',
+        'sale',
+    ],
+    'category' : 'Custom Modules',
+    'description': '''
+A new way of grouping addresses by their country and Zip code using RegEx.
+''',
+    'init_xml' : [
+    ],
+    'demo_xml' : [
+    ],
+    'update_xml' : [
+        'security/ir.model.access.csv',
+        'zip_region_view.xml',
+        'res_partner_view.xml',
+        'sale_view.xml',
+        'stock_view.xml',
+    ],
+    'active': False,
+    'installable': True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ country_zipregion/zip_region.py
@@ -0,0 +1,51 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+
+class zip_region(osv.osv):
+    _name = 'res.zip_region'
+    _description = 'Zip Region'
+    _columns = {
+        'name': fields.char('Region Name', size=64, required=True),
+        'country_id': fields.many2one('res.country', 'Country', required=True),
+        # The regex matching works best with NO index on the address zip field, starting with a ^ clamp, but NOT ending with a $ clamp
+        'zip_regex': fields.char('Zip Regular Expression', size=1024,
+                    help="A PostgreSQL Regular Expression (POSIX) to select ZIP codes. Leave blank to select all."\
+                    "(Example '^CV[[:digit:]]{1,2}[[:blank:]]*[[:digit:]][[:alpha:]]{2}' will select all UK Postcodes for the Coventry region"),
+        'group_ids': fields.many2many('res.zip_region_group', 'res_zip_region_rel', 'region_id', 'region_group_id', 'Zip Region Groups'),
+        }
+    _order = 'name'
+
+zip_region()
+
+class zip_region_group(osv.osv):
+    _name = 'res.zip_region_group'
+    _description = 'Zip Region Group'
+    _columns = {
+        'name': fields.char('Region Group Name', size=64, required=True),
+        'region_ids': fields.many2many('res.zip_region', 'res_zip_region_rel', 'region_group_id', 'region_id', 'Zip Regions'),
+        }
+    _order = 'name'
+
+zip_region_group()
+
