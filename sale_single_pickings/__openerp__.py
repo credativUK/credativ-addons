5f7128ba1453ad08837b240715c56d169c672927
Craig Gowing (credativ) <craig.gowing@credativ.co.uk>
2012-06-06 13:06:20 +0100

    [IMP] Add copyright and licence notices

--- sale_single_pickings/__openerp__.py
+++ sale_single_pickings/__openerp__.py
@@ -17,7 +17,18 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
-import sale
-
+##############################################################################{
+    'name': 'Sale Single Pickings',
+    'version': '1.0',
+    'category': 'Generic Modules/Sales & Purchases',
+    'description': """This Module backports Sale Order action_ship_create from v6.1 and creates single pickings and stock moves per single item.""",
+    'author': 'Credativ',
+    'depends': ['sale'],
+    'init_xml': [],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+    'certificate': '',
+}
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
