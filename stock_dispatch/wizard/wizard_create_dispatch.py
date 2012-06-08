f3d1f08d5966d01eff1a4e2df71bc45c763fff24
Craig Gowing (credativ) <craig.gowing@credativ.co.uk>
2012-06-08 13:20:26 +0100

    [IMP] Update stock_dispatch to use 6.0 wizards and use a new partner category

--- stock_dispatch/wizard/wizard_create_dispatch.py
+++ stock_dispatch/wizard/wizard_create_dispatch.py
@@ -21,87 +21,70 @@
 #
 ##############################################################################
 
-import wizard
 import netsvc
-import pooler
-
 import time
-from osv import osv
+from osv import osv, fields
 from tools.translate import _
 
-create_dispatch_form = '''<?xml version="1.0"?>
-<form string="Create Dispatch">
-  <field name="carrier_id"/>
-  <newline/>
-  <field name="dispatch_date"/>
-</form>
-'''
-fields = {
-        'carrier_id': {
-            'string': 'Carrier',
-            'type': 'many2one',
-            'relation': 'res.partner',
-            'required': True,
-        },
-        'dispatch_date': {
-            'string': 'Planned Dispatch Date',
-            'type': 'date',
-            'default': time.strftime('%Y-%m-%d'),
-            'required': True,
-        }
-}
-
-def _check_moves(self, cr, uid, data, context):
-    pool = pooler.get_pool(cr.dbname)
-    move_string = ''
-    error = False
-
-    for move in pool.get('stock.move').browse(cr, uid, data['ids']):
-        if move.dispatch_id and move.dispatch_id.id != id:
-            error = True
-            move_string += ' (id:%d,dispatch_id:%d)' % (move.id, move.dispatch_id.id)
-
-    if error:
-        raise wizard.except_wizard(_('UserError'), _('One or more moves are already part of another dispatch and' \
-                             ' can not been added to a new one:%s' % (move_string)))
-
-    return data['form']
-
-def _create_dispatch(self, cr, uid, data, context):
-    pool = pooler.get_pool(cr.dbname)
-
-    dispatch_data = {
-                    'carrier_id': data['form']['carrier_id'],
-                    'dispatch_date': data['form']['dispatch_date'],
-                    'stock_moves': ([6, 0, data['ids']],),
-            }
-
-    dispatch = pool.get('stock.dispatch').create(cr, uid, dispatch_data)
-
-    return {
-            'name': 'Dispatch',
-            'view_type': 'form',
-            'view_mode': 'form,tree',
-            'res_model': 'stock.dispatch',
-            'view_id': False,
-            'res_id': dispatch,
-            'type': 'ir.actions.act_window',
-        }
-
-class wizard_create_dispatch(wizard.interface):
-    states = {
-        'init': {
-            'actions': [_check_moves],
-            'result': {'type': 'form', 'arch': create_dispatch_form, 'fields': fields, 'state': [('end', 'Cancel', 'gtk-cancel'), ('create_dispatch', 'Ok', 'gtk-ok')]},
-            },
-        'create_dispatch': {
-            'actions': [],
-            'result': {'type':'action', 'action': _create_dispatch, 'state':'end'}
-        }
+class stock_move_create_dispatch(osv.osv_memory):
+    _name = "stock.move.create_dispatch"
+    _description = "Create Dispatch"
+    _columns = {
+        'move_ids' : fields.text('Stock Moves', required=True), # Store an array of IDs instead of having to make complex osv_memory tables for one2many
+        'carrier_id' : fields.many2one('res.partner', 'Carrier', required=True),
+        'dispatch_date': fields.date('Planned Dispatch Date', required=True),
+    }
+    _defaults = {
+        'dispatch_date': lambda *a: time.strftime('%Y-%m-%d'),
     }
 
-wizard_create_dispatch('stock.move.create_dispatch')
+    def default_get(self, cr, uid, fields, context):
+        move_ids = context and context.get('active_ids', False) or False
+        self._check_moves(cr,uid, move_ids, context)
+        res = super(stock_move_create_dispatch, self).default_get(cr, uid, fields, context=context)
+        res.update({'move_ids': ','.join((str(x) for x in move_ids)) or False})
+        return res
+    
+    def _check_moves(self, cr, uid, move_ids, context):
+        move_string = ''
+        error = False
+        
+        for move in self.pool.get('stock.move').browse(cr, uid, move_ids):
+            if move.dispatch_id:
+                error = True
+                move_string += ' (id:%d,dispatch_id:%d)' % (move.id, move.dispatch_id.id)
+        
+        if error:
+            raise osv.except_osv(_('UserError'), _('One or more moves are already part of another dispatch and' \
+                                ' can not been added to a new one:%s' % (move_string)))
 
+    def create_dispatch(self, cr, uid, ids, context=None):
+        rec_id = context and context.get('active_id', False)
+        assert rec_id, _('Active ID is not set in Context')
+
+        dispatch_obj = self.pool.get('stock.dispatch')
+
+        for data in self.browse(cr, uid, ids, context=context):
+            move_ids = [int(x) for x in data.move_ids.split(',')]
+            self._check_moves(cr,uid, move_ids, context)
+            dispatch_data = {
+                        'carrier_id': data.carrier_id.id,
+                        'dispatch_date': data.dispatch_date,
+                        'stock_moves': ([6, 0, move_ids],),
+                }
+            dispatch = dispatch_obj.create(cr, uid, dispatch_data)
+
+        return {
+                'name': 'Dispatch',
+                'view_type': 'form',
+                'view_mode': 'form,tree',
+                'res_model': 'stock.dispatch',
+                'view_id': False,
+                'res_id': dispatch,
+                'type': 'ir.actions.act_window',
+            }
+
+stock_move_create_dispatch()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
