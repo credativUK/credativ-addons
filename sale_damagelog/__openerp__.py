d8b88488f2741ecce3f09723ea4a6daae8bfb132
Sofia Anwar <sofia.anwar@credativ.in>
2012-07-11 14:49:50 +0530

    upgraded from 6.0 to 6.1

--- sale_damagelog/__openerp__.py
+++ sale_damagelog/__openerp__.py
@@ -5,12 +5,14 @@
     'category': 'Generic Modules/Sales & Purchases',
     'description': """This Module allows you to manage the log for damaged products.""",
     'author': 'Credativ',
-    'depends': ['sale','crm','crm_claim'],
+    'depends': ['sale','crm','crm_claim','account'],
     'init_xml': [],
     'update_xml': [
         'security/ir.model.access.csv',
         'sale_damagelog_view.xml',
-        'wizard/create_damagelog.xml'
+        'wizard/create_damagelog.xml',
+        'sale_comprequest_view.xml',
+        'sale_comprequest_sequence.xml',
     ],
     'demo_xml': [],
     'installable': True,
