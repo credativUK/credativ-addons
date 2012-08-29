b5fe2d698a3b971a2bf839909af8f1f4db544667
Craig Gowing (credativ) <craig.gowing@credativ.co.uk>
2012-08-29 13:45:21 +0100

    [FIX] Add file which was missed in previous merge

--- stock_dispatch/stock_dispatch.py
+++ stock_dispatch/stock_dispatch.py
@@ -21,10 +21,12 @@
 ##############################################################################
 
 from osv import osv, fields
-import time
+import datetime, time
 from osv.orm import except_orm
 from tools.translate import _
 import netsvc
+import pytz
+import tools
 
 class stock_dispatch(osv.osv):
     _name = 'stock.dispatch'
@@ -37,22 +39,43 @@ class stock_dispatch(osv.osv):
         'complete_uid': fields.many2one('res.users', 'Completed User', readonly=True),
         'complete_date': fields.datetime('Completed date', readonly=True),
         'date': fields.datetime('Date created', readonly=True),
-        'state': fields.selection([('draft', 'New'), ('waiting', 'Waiting Another Move'), ('confirmed', 'Waiting Availability'), ('assigned', 'Available'), ('done', 'Done'), ('cancel', 'Cancelled')], 'Status', readonly=True, select=True),
+        'state': fields.selection([('draft', 'Draft'), ('waiting', 'Waiting Another Move'), ('confirmed', 'Confirmed'), ('assigned', 'Available'), ('done', 'Done'), ('cancel', 'Cancelled')], 'Status', readonly=True, select=True),
         'dispatch_date': fields.date('Planned Dispatch Date', required=True, readonly=True, states={'draft':[('readonly',False)]}),
+        'cutoff_date': fields.datetime('Cutoff Date', required=True, readonly=True, states={'draft':[('readonly',False)]}),
         }
 
     _sql_constraints = [
         ('name_uniq', 'unique(name)', 'Dispatch Name must be unique !'),
+        ('cutoff_before_dispatch', 'CHECK (cutoff_before_dispatch<dispatch_date)',  'The cutoff date must occur before the dispatch'),
     ]
+
     _order = 'name desc'
 
     _defaults = {
         'name': lambda obj, cr, uid, context: obj.pool.get('ir.sequence').get(cr, uid, 'stock.dispatch'),
         'state': lambda *a: 'draft',
-        'date': lambda *a: time.strftime('%Y-%m-%d %H:%M:%S'),
-        'dispatch_date': lambda *a: time.strftime('%Y-%m-%d'),
+        'date': lambda *a: datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
+        'dispatch_date': lambda *a: datetime.datetime.utcnow().strftime('%Y-%m-%d'),
     }
 
+    def on_change_dispatch_date(self, cr, uid, id, dispatch_date, context=None):
+        res = {'value': {}}
+        if dispatch_date:
+            tz = self.pool.get('res.users').read(cr, uid, uid, ['context_tz'])['context_tz']
+            cutoff = (datetime.datetime.strptime(dispatch_date, '%Y-%m-%d') - datetime.timedelta(days=1)).replace(hour=15, minute=0, second=0)
+            # FIXME: DST not working correctly in OpenERP - no idea why it is failing, but I have resorted to hard coding European DST date ranges to get the default value
+            if tz == 'GMT':
+                dst_start = 31 - ((((5 * cutoff.year) / 4) + 4) % 7)
+                dst_end = 31 - ((((5 * cutoff.year) / 4) + 1) % 7)
+                if ((cutoff.month == 3 and cutoff.day >= dst_start) or
+                   (cutoff.month > 3 and cutoff.month < 10) or
+                   (cutoff.month == 10 and cutoff.day <= dst_end)):
+                    cutoff = cutoff.replace(hour=14)
+            # FIXME: End
+            cutoff = cutoff.strftime('%Y-%m-%d %H:%M:%S')
+            res['value']['cutoff_date'] = tools.server_to_local_timestamp(cutoff, '%Y-%m-%d %H:%M:%S', '%Y-%m-%d %H:%M:%S', tz)
+        return res
+        
     def on_change_stock_moves(self, cr, uid, ids, stock_moves, context=None):
         move_list = []
         move_string = ''
@@ -86,10 +109,6 @@ class stock_dispatch(osv.osv):
         if context is None:
             context = {}
 
-        logger = netsvc.Logger()
-        start_time = time.time()
-        logger.notifyChannel('stock.dispatch', netsvc.LOG_DEBUG, 'action_done: start %s' % ids)
-        
         for dispatch in self.browse(cr, uid, ids):
             context['from_dispatch'] = dispatch.id
             move_ids = [x.id for x in dispatch.stock_moves]
@@ -103,8 +122,7 @@ class stock_dispatch(osv.osv):
                     # cause workflow to update since we called move.action_done
                     wkf_service.trg_write(uid, 'stock.picking', move.picking_id.id, cr)
 
-        self.write(cr, uid, ids, {'state': 'done', 'complete_date': time.strftime('%Y-%m-%d %H:%M:%S'), 'complete_uid': uid})
-        logger.notifyChannel('stock.dispatch', netsvc.LOG_DEBUG, 'action_done: finished in %s' % (time.time() - start_time, ))
+        self.write(cr, uid, ids, {'state': 'done', 'complete_date': datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'), 'complete_uid': uid})
         return True
 
     def action_cancel(self, cr, uid, ids, context=None):
