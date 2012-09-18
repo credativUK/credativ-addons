32793a4c61cc2e22c0ee231b6e4eddf413e7e465
Craig Gowing (credativ) <craig.gowing@credativ.co.uk>
2012-09-18 09:56:52 +0100

    [ADD] New module res_partner_fraud which adds a manual field for flagging a partner and all their sale orders a fraud

---
+++ res_partner_fraud/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name' : 'Partner Fraud Flag',
+    'version' : '1.0.0.0',
+    'author' : 'credativ',
+    'website' : 'http://credativ.co.uk',
+    'depends' : [
+        'base',
+        'sale',
+    ],
+    'category' : 'Custom Modules',
+    'description': '''
+A manual flag for Fraud set on Partners which will show in Sale Orders
+''',
+    'init_xml' : [
+    ],
+    'demo_xml' : [
+    ],
+    'update_xml' : [
+        'res_partner_view.xml',
+        'sale_view.xml',
+    ],
+    'active': False,
+    'installable': True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
