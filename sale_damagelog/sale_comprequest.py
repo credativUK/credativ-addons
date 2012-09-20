619cfc9c85b20d67bfa955b205a7ba65ad5e0849
Craig Gowing (credativ) <craig.gowing@credativ.co.uk>
2012-09-20 09:28:26 +0100

    [IMP] Add jump links and extra list views for compensation requests

--- sale_damagelog/sale_comprequest.py
+++ sale_damagelog/sale_comprequest.py
@@ -2,7 +2,6 @@ from osv import osv,fields
 import time
 
 class sale_comprequest(osv.osv):
-
     _name = 'sale.comprequest'
 
     _columns = {
@@ -16,7 +15,7 @@ class sale_comprequest(osv.osv):
         'cancel_uid': fields.many2one('res.users', 'Last Updated By', readonly=True),
         'confirm_date': fields.datetime('Last Updated', readonly=True),
         'confirm_uid': fields.many2one('res.users', 'Last Updated By', readonly=True),
-        'sale_order_id': fields.related('damagelog_id', 'stock_move_id', 'sale_line_id', 'order_id', type='many2one', relation='sale.order', string='Order Reference', readonly=True),
+        'sale_order_id': fields.related('damagelog_id', 'stock_move_id', 'sale_line_id', 'order_id', type='many2one', relation='sale.order', string='Order Reference', readonly=True, store=True),
         'date_order': fields.related('sale_order_id', 'date_order', type='date', string='Order Date', readonly=True),
         'product_id': fields.related('damagelog_id', 'stock_move_id', 'product_id', type='many2one', relation='product.product', string='Product', readonly=True),
         'product_sku': fields.related('product_id', 'default_code', type='char', size=16, string='Product Code', readonly=True),
@@ -97,3 +96,12 @@ class sale_comprequest_comment(osv.osv):
     }
 
 sale_comprequest_comment()
+
+class sale_order(osv.osv):
+    _inherit = 'sale.order'
+    
+    _columns = {
+        'comp_reqs': fields.one2many('sale.comprequest', 'sale_order_id', 'Compensation Requests', readonly=True),
+        }
+
+sale_order()
