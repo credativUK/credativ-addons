016347f7e3c15f21ba08c10819cfddc2c841d306
Derek Sheridan <derek.sheridan@credativ.co.uk>
2012-10-11 12:03:06 +0100

    [IMP] terp file updated - product_designer

--- product_designer/__terp__.py
+++ product_designer/__terp__.py
@@ -22,7 +22,7 @@
 {
     'name' : 'Product Designer',
     'version' : '1.0.0.0',
-    'author' : 'credativ',
+    'author' : 'credativ Ltd',
     'website' : 'http://credativ.co.uk',
     'depends' : [
         'base', 
