49181a91d4cd96d7ae39e3e21fe8f70574e5d704
Derek Sheridan <derek.sheridan@credativ.co.uk>
2012-10-11 12:39:39 +0100

    [IMP] terp file updated - res_partner_fraud

--- res_partner_fraud/__openerp__.py
+++ res_partner_fraud/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name' : 'Partner Fraud Flag',
     'version' : '1.0.0.0',
-    'author' : 'credativ',
+    'author' : 'credativ Ltd',
     'website' : 'http://credativ.co.uk',
     'depends' : [
         'base',
