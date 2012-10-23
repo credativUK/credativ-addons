b18ddca820ce0d728fb6234416db937f569aff9e
Derek Sheridan <derek.sheridan@credativ.co.uk>
2012-10-23 10:13:06 +0100

    [IMP] terp file updated - mrp_auto_from_stock_move

--- mrp_auto_from_stock_move/__openerp__.py
+++ mrp_auto_from_stock_move/__openerp__.py
@@ -1,9 +1,29 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
     "name" : "Delivery Driven Automatic Manufacturing",
     "version" : "1.1",
-    "author" : "credativ",
-    "website" : "http://www.credativ.com",
+    "author" : "credativ Ltd",
+    'website' : 'http://credativ.co.uk',
     "category" : "Manufacturing",
     "sequence": 18,
     "images" : [],
