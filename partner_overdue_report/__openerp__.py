710c289965bbcfb18827c247a541a5d42ed0c02d
Derek Sheridan <derek.sheridan@credativ.co.uk>
2012-11-07 11:45:28 +0000

    [IMP] updated terp

--- partner_overdue_report/__openerp__.py
+++ partner_overdue_report/__openerp__.py
@@ -1,21 +1,21 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2012 credativ ltd (<http://www.credativ.co.uk>). All Rights Reserved
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
@@ -26,8 +26,8 @@
 Report showing the outstanding debt of partners from a particular date to a particular date
 
      """,
-    'author': 'Credativ',
-    'website': 'http://www.credativ.com',
+    'author': 'credativ Ltd',
+    'website': 'http://credativ.co.uk',
     'depends': ['account'],
     'init_xml': [
 ],
