21fb8aa6e96bc0091116e09c9c277838599ea9f7
Craig Gowing (credativ) <craig.gowing@credativ.co.uk>
2013-05-17 13:50:08 +0100

    [ADD] New module to ignore stock moves from procurements for draft POs from being included in virtual stock

---
+++ po_ignore_draft_procurement/product.py
@@ -0,0 +1,184 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C) 2013 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from osv import fields, osv
+
+
+class product_product(osv.osv):
+    _inherit ='product.product'
+
+    def get_product_draft_procurements(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        
+        location_obj = self.pool.get('stock.location')
+        warehouse_obj = self.pool.get('stock.warehouse')
+        shop_obj = self.pool.get('sale.shop')
+        
+        states = context.get('states',[])
+        what = context.get('what',())
+        if not ids:
+            ids = self.search(cr, uid, [])
+        res = {}.fromkeys(ids, 0.0)
+        if not ids:
+            return res
+
+        if context.get('shop', False):
+            warehouse_id = shop_obj.read(cr, uid, int(context['shop']), ['warehouse_id'])['warehouse_id'][0]
+            if warehouse_id:
+                context['warehouse'] = warehouse_id
+
+        if context.get('warehouse', False):
+            lot_id = warehouse_obj.read(cr, uid, int(context['warehouse']), ['lot_stock_id'])['lot_stock_id'][0]
+            if lot_id:
+                context['location'] = lot_id
+
+        if context.get('location', False):
+            if type(context['location']) == type(1):
+                location_ids = [context['location']]
+            elif type(context['location']) in (type(''), type(u'')):
+                location_ids = location_obj.search(cr, uid, [('name','ilike',context['location'])], context=context)
+            else:
+                location_ids = context['location']
+        else:
+            location_ids = []
+            wids = warehouse_obj.search(cr, uid, [], context=context)
+            for w in warehouse_obj.browse(cr, uid, wids, context=context):
+                location_ids.append(w.lot_stock_id.id)
+
+        # build the list of ids of children of the location given by id
+        if context.get('compute_child',True):
+            child_location_ids = location_obj.search(cr, uid, [('location_id', 'child_of', location_ids)])
+            location_ids = child_location_ids or location_ids
+        
+        # this will be a dictionary of the UoM resources we need for conversion purposes, by UoM id
+        uoms_o = {}
+        # this will be a dictionary of the product UoM by product id
+        product2uom = {}
+        for product in self.browse(cr, uid, ids, context=context):
+            product2uom[product.id] = product.uom_id.id
+            uoms_o[product.uom_id.id] = product.uom_id
+
+        results = []
+        results2 = []
+
+        from_date = context.get('from_date',False)
+        to_date = context.get('to_date',False)
+        date_str = False
+        date_values = False
+        where = [tuple(location_ids),tuple(location_ids),tuple(ids),tuple(states)]
+        if from_date and to_date:
+            date_str = "sm.date>=%s and sm.date<=%s"
+            where.append(tuple([from_date]))
+            where.append(tuple([to_date]))
+        elif from_date:
+            date_str = "sm.date>=%s"
+            date_values = [from_date]
+        elif to_date:
+            date_str = "sm.date<=%s"
+            date_values = [to_date]
+        if date_values:
+            where.append(tuple(date_values))
+
+        prodlot_id = context.get('prodlot_id', False)
+        prodlot_clause = ''
+        if prodlot_id:
+            prodlot_clause = ' and sm.prodlot_id = %s '
+            where += [prodlot_id]
+        elif 'prodlot_id' in context and not prodlot_id:
+            prodlot_clause = 'and sm.prodlot_id is null '
+        
+        where += [('draft', 'cancel', 'confirmed'),]
+        
+        # TODO: perhaps merge in one query.
+        if 'in' in what:
+            # all moves from a location out of the set to a location in the set
+            cr.execute(
+                'select sum(sm.product_qty), sm.product_id, sm.product_uom '\
+                'from stock_move sm '\
+                'inner join procurement_order pr on pr.move_id = sm.id '\
+                'inner join purchase_order po on po.id = pr.purchase_id '\
+                'where sm.location_id NOT IN %s '\
+                'and sm.location_dest_id IN %s '\
+                'and sm.product_id IN %s '\
+                'and sm.state IN %s' + (date_str and 'and '+date_str+' ' or '') +' '\
+                + prodlot_clause + 
+                'and po.state NOT IN %s '\
+                'group by sm.product_id,sm.product_uom',tuple(where))
+            results = cr.fetchall()
+        if 'out' in what:
+            # all moves from a location in the set to a location out of the set
+            cr.execute(
+                'select sum(sm.product_qty), sm.product_id, sm.product_uom '\
+                'from stock_move sm '\
+                'inner join procurement_order pr on pr.move_id = sm.id '\
+                'inner join purchase_order po on po.id = pr.purchase_id '\
+                'where sm.location_id IN %s '\
+                'and sm.location_dest_id NOT IN %s '\
+                'and sm.product_id  IN %s '\
+                'and sm.state in %s ' + (date_str and 'and '+date_str+' ' or '') + ' '\
+                + prodlot_clause + 
+                'and po.state NOT IN %s '\
+                'group by sm.product_id,sm.product_uom',tuple(where))
+            results2 = cr.fetchall()
+            
+        # Get the missing UoM resources
+        uom_obj = self.pool.get('product.uom')
+        uoms = map(lambda x: x[2], results) + map(lambda x: x[2], results2)
+        if context.get('uom', False):
+            uoms += [context['uom']]
+        uoms = filter(lambda x: x not in uoms_o.keys(), uoms)
+        if uoms:
+            uoms = uom_obj.browse(cr, uid, list(set(uoms)), context=context)
+            for o in uoms:
+                uoms_o[o.id] = o
+                
+        #TOCHECK: before change uom of product, stock move line are in old uom.
+        context.update({'raise-exception': False})
+        # Count the incoming quantities
+        for amount, prod_id, prod_uom in results:
+            amount = uom_obj._compute_qty_obj(cr, uid, uoms_o[prod_uom], amount,
+                     uoms_o[context.get('uom', False) or product2uom[prod_id]], context=context)
+            res[prod_id] += amount
+        # Count the outgoing quantities
+        for amount, prod_id, prod_uom in results2:
+            amount = uom_obj._compute_qty_obj(cr, uid, uoms_o[prod_uom], amount,
+                    uoms_o[context.get('uom', False) or product2uom[prod_id]], context=context)
+            res[prod_id] -= amount
+        return res
+
+    def get_product_available(self, cr, uid, ids, context=None):
+        if not ids:
+            ids = self.search(cr, uid, [])
+        res = {}.fromkeys(ids, 0.0)
+        if not ids:
+            return res
+
+        res1 = super(product_product, self).get_product_available(cr, uid, ids, context=context)
+        res2 = self.get_product_draft_procurements(cr, uid, ids, context=context)
+        
+        for id in ids:
+            res[id] = res1.get(id, 0.0) - res2.get(id, 0.0)
+        
+        return res
+    
+product_product()
