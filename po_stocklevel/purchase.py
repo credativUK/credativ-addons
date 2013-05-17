d4a5f7141f8dd36869d76ba9c0677a3aefc93dab
Craig Gowing (credativ) <craig.gowing@credativ.co.uk>
2013-05-17 08:02:21 +0100

    [FIX] Remove code duplication for getting stock levels. Fix on_change issues and problem when mirroring stock levels to the PO tree view.

--- po_stocklevel/purchase.py
+++ po_stocklevel/purchase.py
@@ -19,43 +19,42 @@
 #
 ##############################################################################
 
-from osv import osv,fields
-from collections import defaultdict
+from osv import osv, fields
 import decimal_precision as dp
+
+class purchase_order(osv.osv):
+    _inherit = 'purchase.order'
+    
+    def onchange_po_lines(self, cr, uid, ids, order_lines, context=None):
+        import ipdb; ipdb.set_trace()
+        return {}
+    
+purchase_order()
 
 class purchase_order_line(osv.osv):
     _inherit = 'purchase.order.line'
     
-    def _get_product_available(self,cr,uid,ids,context=None):
-        '''Method to get product stock '''
-                
-        return self.pool.get('product.product').\
-                    get_product_available(cr, uid, ids, context=context)
-    
-    def _product_available(self, cr, uid, ids, field_names=None, arg=False, context=None):
-        """ Finds the incoming and outgoing quantity of product.
-        @return: Dictionary of values
-        """
-
-        stock_mapping = {'qty_available':'out','incoming_qty':'in'}
-        
-        if not field_names:
-            field_names = False
-        if context is None:
-            context = {} 
+    def _get_product_qty(self, cr, uid, ids, field_names=None, args=False, context=None):
+        """Required in order to show 0.0 instead of an empty string when using a related field"""
+        product_obj = self.pool.get('product.product')
         res = {}
         
+        if not field_names:
+            return res
+        
         for id in ids:
-            if field_names not in stock_mapping.keys():
-                continue
-            c = context.copy()
-            if field_names == 'qty_available':
-                c.update({ 'states': ('done',), 'what': ('in', 'out') })
-            else:
-                c.update({ 'states': ('confirmed','waiting','assigned'), 'what': ('in',) })
-            product_id = self.browse(cr,uid,id,context=context).product_id.id
-            stock = self._get_product_available(cr, uid, [product_id], context=c)
-            res[id] = stock.get(product_id,0.0)
+            res[id] = dict(zip(field_names, [0.0,]*len(field_names)))
+        
+        for pol in self.browse(cr, uid, ids, context=context):
+            for field_name in field_names:
+                if field_name[-6:] == '_dummy':
+                    field_name_read = field_name[:-6]
+                else:
+                    field_name_read = field_name
+                if pol.product_id and hasattr(pol.product_id, field_name_read):
+                    res[id][field_name] = getattr(pol.product_id, field_name_read) or 0.0
+                else:
+                    res[id][field_name] = 0.0
         
         return res
     
@@ -63,34 +62,34 @@ class purchase_order_line(osv.osv):
             partner_id, date_order=False, fiscal_position_id=False, date_planned=False,
             name=False, price_unit=False, notes=False, context=None):
         '''Product onchange overridden to pass values of stock on hand and Incoming Stock'''
+        product_obj = self.pool.get('product.product')
         
-        qty_dic = {'qty_available':0.0,'incoming_qty':0.0}
-        mapping_dic = {'qty_available':{ 'states': ('done',), 'what': ('in', 'out') },\
-                       'incoming_qty':{ 'states': ('confirmed','waiting','assigned'), 'what': ('in',) }}
-        
-        if context is None:
-            context = {}
-        
+        qty_dic={'incoming_qty': 0.0, 'qty_available': 0.0}
         if product_id:
-            for avail_type in mapping_dic.keys():
-                c = context.copy()
-                c.update(mapping_dic[avail_type])
-                stock = self._get_product_available(cr, uid, [product_id], context=c)
-                qty_dic[avail_type] = stock.get(product_id,0.0)
+            stock = product_obj.read(cr, uid, product_id, ['incoming_qty', 'qty_available'], context=context)
+            qty_dic['incoming_qty'] = stock.get('incoming_qty', 0.0)
+            qty_dic['qty_available'] = stock.get('qty_available', 0.0)
+            qty_dic['incoming_qty_dummy'] = stock.get('incoming_qty', 0.0)
+            qty_dic['qty_available_dummy'] = stock.get('qty_available', 0.0)
             
         res_values =  super(purchase_order_line,self).onchange_product_id(cr, uid, ids, pricelist_id, product_id, qty, uom_id,
             partner_id, date_order=date_order, fiscal_position_id=fiscal_position_id, date_planned=date_planned,
             name=name, price_unit=price_unit, notes=notes, context=context)
         
-        res_values['value'].update(qty_dic)
+        res_values.setdefault('value', {}).update(qty_dic)
         
         return res_values
     
+    product_id_change = onchange_product_id
     
+    # _dummy fields are required as a work around where readonly fields are not copied from the form to the tree for
+    # PO lines. We have to mirror the fields so one is readonly and the other is editable and invisible
     _columns = {
-        'incoming_qty': fields.function(_product_available, type='float', string='Incoming Stock',digits_compute=dp.get_precision('Product UoM'),help='Incoming Product Stock'),
-        'qty_available': fields.function(_product_available, type='float', string='On Hand Stock',digits_compute=dp.get_precision('Product UoM'),help='Product Stock on hand'),
-                }
+        'incoming_qty': fields.function(_get_product_qty, method=True, type='float', string='Incoming Stock', digits_compute=dp.get_precision('Product UoM'), help='Incoming Product Stock', multi='_get_product_qty',),
+        'qty_available': fields.function(_get_product_qty, method=True, type='float', string='On Hand Stock', digits_compute=dp.get_precision('Product UoM'), help='Product Stock on hand', multi='_get_product_qty',),
+        'incoming_qty_dummy': fields.function(_get_product_qty, method=True, type='float', string='Incoming Stock', digits_compute=dp.get_precision('Product UoM'), help='Incoming Product Stock', multi='_get_product_qty',),
+        'qty_available_dummy': fields.function(_get_product_qty, method=True, type='float', string='On Hand Stock', digits_compute=dp.get_precision('Product UoM'), help='Product Stock on hand', multi='_get_product_qty',),
+        }
     
 purchase_order_line()
 
