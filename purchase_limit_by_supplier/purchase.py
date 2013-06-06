c5a8ab7d631dd51cf2440622b78716d3218263ea
Jacob Hicks <jacob.hicks@credativ.co.uk>
2013-06-06 17:32:56 +0100

    [ADD] New module to filter products by supplier on purchase order lines

---
+++ purchase_limit_by_supplier/purchase.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+
+class purchase_order_line(osv.osv):
+    _inherit = 'purchase.order.line'
+    
+    _columns = {
+        'list_all_products': fields.boolean('List all Products', help='By default the products available will only be ones available from the selected supplier. This option will show all products available instead.'),
+    }
+
+purchase_order_line()
