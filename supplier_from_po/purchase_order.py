f27d42e81c754e295c6010fe074b9a0e1d79901e
Matthieu Choplin matthieuchoplin@hotmail.com <>
2013-07-10 11:47:24 +0100

    [ERPSUP-273] check if a seller exists before browsing it

--- supplier_from_po/purchase_order.py
+++ supplier_from_po/purchase_order.py
@@ -38,7 +38,7 @@ class purchase_order(osv.osv):
                 # check if supplier already exists in the supplier list for this product
                 if partner_id not in [x.name.id for x in product.seller_ids]:
                     # add supplier to supplierinfo for product
-                    next_sequence =  product.seller_ids and max([sup.sequence for sup in product.seller_ids]) or 0 + 1
+                    next_sequence =  (product.seller_ids and max([sup.sequence for sup in product.seller_ids]) or 0) + 1
                     supplier_vals = {'name': partner_id,
                                     'product_id': product.id,
                                     'min_qty': 0,
