a9dd7315f7a74fd972dac4116003a309b74378c7
Jacob Hicks <jacob.hicks@credativ.co.uk>
2013-09-06 10:04:45 +0100

    [ADD] New module to override validations on certain products

---
+++ validation_products/product.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+
+class product_product(osv.osv):
+    _inherit = 'product.product'
+
+    _columns = {
+        'validate_override': fields.boolean('Validation Over-ride'),
+        }
+
+    def check_validation_rule(self, cr, uid, ids, opp, context=None):
+        new_ids = list(ids)
+        for product in self.browse(cr, uid, ids, context=context):
+            if product.validate_override:
+                new_ids.remove(product.id)
+        return super(product_product, self).check_validation_rule(cr, uid, new_ids, opp=opp, context=context)
+
+    _defaults = {
+        'validate_override': False,
+    }
+
+product_product()
