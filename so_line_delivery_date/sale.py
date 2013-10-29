2109ec2d00d60ce5f0d85e9819eda99f7756d769
Jacob Hicks <jacob.hicks@credativ.co.uk>
2013-10-29 11:17:50 +0000

    [IMP] Improve onchange handlers and sale order confirmation logic

--- so_line_delivery_date/sale.py
+++ so_line_delivery_date/sale.py
@@ -46,58 +46,85 @@ sale_order()
 class sale_order_line(osv.osv):
     _inherit = 'sale.order.line'
 
-    def _fnct_days_until_delivery(self, cr, uid, ids, field_name, arg, context=None):
-        res = {}
-        for line in self.browse(cr, uid, ids, context=context):
-            try:
-                date_confirm = line.order_id.date_confirm and datetime.datetime.strptime(line.order_id.date_confirm, '%Y-%m-%d')
-                timedelta_until_delivery = line.requested_delivery_date and datetime.datetime.strptime(line.requested_delivery_date, '%Y-%m-%d') - ( date_confirm or datetime.datetime.now() )
-                days_until_delivery = timedelta_until_delivery and timedelta_until_delivery.days + 1 # Add one because of rounding
-                res[line.id] = days_until_delivery
-            except:
-                res[line.id] = False
+    def onchange_delay(self, cr, uid, ids, delay, context=None):
+        context = context or {}
+        res = {'value':{}}
+        res['value']['default_delivery_date'] = self._get_single_default_delivery_date(cr, uid, ids and ids[0], delay=delay, context=context)
         return res
 
-    def _fnct_default_delivery_date(self, cr, uid, ids, field_name, arg, context=None):
+    def _get_single_days_until_delivery(self, cr, uid, id, requested_delivery_date, context=None):
+        today = datetime.datetime(year=datetime.datetime.today().year, month=datetime.datetime.today().month, day=datetime.datetime.today().day)
+        if requested_delivery_date and not id:
+            timedelta_until_delivery = datetime.datetime.strptime(requested_delivery_date, '%Y-%m-%d') - today
+            return timedelta_until_delivery and timedelta_until_delivery.days
+        try:
+            line = self.browse(cr, uid, id, context=context)
+            if not requested_delivery_date:
+                requested_delivery_date = line.requested_delivery_date
+            date_confirm = line.order_id.date_confirm and datetime.datetime.strptime(line.order_id.date_confirm, '%Y-%m-%d')
+            timedelta_until_delivery = datetime.datetime.strptime(requested_delivery_date, '%Y-%m-%d') - ( date_confirm or today )
+            return timedelta_until_delivery and timedelta_until_delivery.days
+        except:
+            return False
+
+    def _fnct_days_until_delivery(self, cr, uid, ids, field_name, arg, context=None):
+        context = context or {}
         res = {}
         for line in self.browse(cr, uid, ids, context=context):
-            try:
-                date_confirm = line.order_id.date_confirm and datetime.datetime.strptime(line.order_id.date_confirm, '%Y-%m-%d')
-                default_delivery_datetime = datetime.timedelta(days=line.delay) + ( date_confirm or datetime.datetime.now() )
-                res[line.id] =  default_delivery_datetime.strftime('%Y-%m-%d')
-            except:
-                res[line.id] = False
+            res[line.id] = self._get_single_days_until_delivery(cr, uid, line.id, line.requested_delivery_date, context=context)
+        return res
+
+    def _get_single_default_delivery_date(self, cr, uid, id, delay, context=None):
+        today = datetime.datetime(year=datetime.datetime.today().year, month=datetime.datetime.today().month, day=datetime.datetime.today().day)
+        if delay and not id:
+            default_delivery_datetime = datetime.timedelta(days=delay) + today
+            return default_delivery_datetime.strftime('%Y-%m-%d')
+        try:
+            line = self.browse(cr, uid, id, context=context)
+            if not delay:
+                delay = line.delay
+            date_confirm = line.order_id.date_confirm and datetime.datetime.strptime(line.order_id.date_confirm, '%Y-%m-%d')
+            default_delivery_datetime = datetime.timedelta(days=delay) + ( date_confirm or today )
+            return default_delivery_datetime.strftime('%Y-%m-%d')
+        except:
+            return False
+        
+    def _fnct_default_delivery_date(self, cr, uid, ids, field_name, arg, context=None):
+        context = context or {}
+        res = {}
+        for line in self.browse(cr, uid, ids, context=context):
+            if line.state == 'confirmed':
+                res[line.id] = line.default_delivery_date_when_confirmed
+            else:
+                res[line.id] = self._get_single_default_delivery_date(cr, uid, line.id, delay=line.delay, context=context)
         return res
 
     def onchange_requested_delivery_date(self, cr, uid, ids, requested_delivery_date, default_delivery_date, context=None):
         if not context:
             context = {}
-        
+        if not isinstance(ids, list):
+            ids = [ids]
         res = {}
-        new_days_until_delivery = False
-        if requested_delivery_date:
-            line = self.browse(cr, uid, ids, context=context)[0]
-            original_date = line.requested_delivery_date and datetime.datetime.strptime(line.requested_delivery_date, '%Y-%m-%d') or datetime.datetime.today()
-            days_change =  ( datetime.datetime.strptime(requested_delivery_date, '%Y-%m-%d') - original_date ).days
-            new_days_until_delivery = (line.days_until_delivery or 0) + days_change
-            if requested_delivery_date < default_delivery_date:
-                res['warning'] = {'title':'Requested days until delivery is less than delivery lead time', 'message':'Please ensure this delivery date is manageable before continuing.'}
-        res['value'] = {'days_until_delivery': new_days_until_delivery }
-        
+        res = {'value':{}}
+        res['value']['days_until_delivery'] = self._get_single_days_until_delivery(cr, uid, ids and ids[0], requested_delivery_date=requested_delivery_date, context=None)
+        if requested_delivery_date < default_delivery_date:
+            res['warning'] = {'title':'Requested days until delivery is less than delivery lead time', 'message':'Please ensure this delivery date is manageable before continuing.'}
         return res
 
     def button_confirm(self, cr, uid, ids, context=None):
         if not context:
             context = {}
-        res = super(sale_order_line, self).button_confirm(cr, uid, ids, context=context)
         for line in self.browse(cr, uid, ids, context=context):
+            self.write(cr, uid, ids, {'default_delivery_date_when_confirmed': line.default_delivery_date})
             if line.days_until_delivery:
                 self.write(cr, uid, ids, {'delay': line.days_until_delivery})
+        res = super(sale_order_line, self).button_confirm(cr, uid, ids, context=context)
         return res
 
     _columns = { 
         'days_until_delivery': fields.function(_fnct_days_until_delivery, string='Days Until Delivery', type='float', help="Number of days remaining until requested delivery date.", readonly=True),
         'requested_delivery_date': fields.date('Requested Delivery Date', select=True, help="Date on which customer has requested delivery."),
+        'default_delivery_date_when_confirmed': fields.date('Default Delivery Date When Confirmed'),
         'default_delivery_date': fields.function(_fnct_default_delivery_date, string='Default Delivery Date', type='date', help="Date on which delivery is projected, assuming customer has not requested a specific date."),
     }
     _defaults = {
