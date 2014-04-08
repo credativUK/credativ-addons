4e1133d9f23665ae1f1e490a625178da4213b5f7
Jacob Hicks <jacob.hicks@credativ.co.uk>
2014-04-08 09:36:13 +0100

    [ADD] New modules to allow customising order prefixes

---
+++ purchase_company_prefix/purchase.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+
+class purchase_order(osv.osv):
+    _inherit='purchase.order'
+
+    def create(self, cr, uid, vals, context=None):
+        if vals.get('name','/')=='/':
+            company = self.pool.get('res.company').browse(cr, uid, vals.get('company_id', []), context=context)
+            prefix = company and company.purchase_prefix or ''
+            sequence = self.pool.get('ir.sequence').get(cr, uid, 'purchase.order') or '/'
+            vals['name'] = '%s%s' % (prefix, sequence)
+        order =  super(purchase_order, self).create(cr, uid, vals, context=context)
+        return order
+
+purchase_order()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ sale_order_shop_prefix/sale.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+
+class sale_shop(osv.osv):
+    _inherit='sale.shop'
+
+    _columns = {
+        'sale_prefix': fields.char('Sale Prefix', size=64, help="Appears at the start of sale order names for this shop."),
+    }
+
+sale_shop()
+
+class sale_order(osv.osv):
+    _inherit='sale.order'
+
+    def create(self, cr, uid, vals, context=None):
+        if vals.get('name','/')=='/':
+            shop = self.pool.get('sale.shop').browse(cr, uid, vals.get('shop_id', []), context=context)
+            prefix = shop and shop.sale_prefix or ''
+            sequence = self.pool.get('ir.sequence').get(cr, uid, 'sale.order') or '/'
+            vals['name'] = '%s%s' % (prefix, sequence)
+        order =  super(sale_order, self).create(cr, uid, vals, context=context)
+        return order
+
+sale_order()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
