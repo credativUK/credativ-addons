127f9a75f3d6f42c3a041a3132c859f1eed00713
Kinner Vachhani <kin.vachhani@gmail.com>
2014-05-21 10:16:42 +0100

    [ADD] New module sale order line tax filter

---
+++ sale_order_line_tax_filter/sale.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution   
+#    Copyright (C) 2014 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv
+
+class sale_order(osv.osv):
+    _inherit = "sale.order"
+
+    def onchange_shop_id(self, cr, uid, ids, shop_id, context=None):
+        '''Change company_id on changing shop'''
+
+        res = super(sale_order,self).onchange_shop_id(cr, uid, ids, shop_id, context=context)
+        v = res.get('value',{})
+        if shop_id:
+            shop = self.pool.get('sale.shop').browse(cr, uid, shop_id, context=context)
+            if shop.company_id:
+                v['company_id'] = shop.company_id.id
+        return {'value':v}
+
+sale_order()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
