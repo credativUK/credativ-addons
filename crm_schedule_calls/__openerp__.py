378b5626f4ad89bff567378146934d9d95ccdc48
Craig Gowing (credativ) <craig.gowing@credativ.co.uk>
2014-05-29 16:06:10 +0100

    [IMP] Module to allow scheduling multiple calls for multiple leads

---
+++ crm_schedule_calls/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution   
+#    Copyright (C) 2014 credativ ltd (<http://www.credativ.co.uk>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'CRM Calls from Leads',
+    'version': '1.0',
+    'category': 'Customer Relationship Management',
+    'description':
+        """
+        Calls from Leads allow multiple calls to be scheduled from a selection of multiple leads or opportunities.
+        """,
+    'author': 'credativ Ltd',
+    'website' : 'http://credativ.co.uk',
+    'depends': [
+        'crm',
+        ],
+    'init_xml': [
+        ],
+    'update_xml': [
+        'wizard/crm_calls_wizard_view.xml',
+    ],
+    'demo_xml': [
+    ],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
