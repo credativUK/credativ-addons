d9b47511d045bccbc133cb336c6f9206000a6045
tornoz <tornoz@laposte.net>
2014-07-09 14:36:54 +0100

    [ADD]Adds a category to the sales order

---
+++ sale_order_category/sale_order.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+
+
+class sale_order_category(osv.osv):
+    _name = "sale.order.category"
+    _description = "Sale Order Category"
+    _columns = {
+        'name': fields.char('Name', size=64, required=True, translate=True, select=True),
+    }
+
+
+class sale_order(osv.osv):
+    _inherit='sale.order'
+    
+    _columns = {
+        'categ_id': fields.many2one('sale.order.category','Category', required=True, change_default=True,help="Select category for the current sale order"),
+    }
+
+sale_order()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
