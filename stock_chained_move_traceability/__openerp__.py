01c6447c0c35927338e38ed087e70fdc8b28eab5
Tom Pickering <tom.pickering@credativ.co.uk>
2014-10-28 10:13:52 +0000

    [ADD] Module which displays chained moves on Stock Move forms in pickings.

---
+++ stock_chained_move_traceability/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name' : 'Chained Move Traceability',
+    'version' : '1.0.0',
+    'author' : 'credativ',
+    'website' : 'http://credativ.co.uk',
+    'depends' : [
+        'stock',
+    ],
+    'data' : [
+    ],
+    'category' : 'Stock Management',
+    'description': '''
+Add an element to the Stock Move form view which displays any
+related moves created due to chained locations.
+''',
+    'init_xml' : [
+    ],
+    'demo_xml' : [],
+    'update_xml' : [
+        'stock_view.xml',
+    ],
+    'installable': True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ stock_chained_move_traceability/stock.py
@@ -0,0 +1,55 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 credativ Ltd (<http://credativ.co.uk>).
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+
+class StockMove(osv.Model):
+    _inherit = 'stock.move'
+
+
+    def _find_related_moves(self, cr, uid, id, relation, context=None):
+        moves = []
+        inverse_relation = relation == 'child_id' and 'parent_id' or 'child_id'
+        sql = 'SELECT %s FROM stock_move_history_ids WHERE %s = %s'
+        cr.execute(sql, (relation, inverse_relation, str(id)))
+        res = cr.fetchall()
+        if res:
+            moves.append(res[0][0])
+            moves.extend(self._find_related_moves(cr, uid, res[0][0], relation, context=context))
+        return moves
+
+
+    def _calc_chained_moves(self, cr, uid, ids, fields, arg, context=None):
+        chained_moves = {}
+        ids = isinstance(ids, list) and ids or [ids]
+        for id in ids:
+            chained_moves[id] = []
+            for relation in ['child_id', 'parent_id']:
+                chained_moves[id].extend(self._find_related_moves(cr, uid, id, relation, context=context))
+        return chained_moves
+
+
+    _columns = {
+            'chained_moves' : fields.function(_calc_chained_moves, type='many2many', relation='stock.move', string='Chained Moves'),
+    }
+
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
