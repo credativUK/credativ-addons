818f4d4af37f4808c540ca0432729876141369ae
Tom Pickering <tom.pickering@credativ.co.uk>
2014-11-14 13:57:43 +0000

    [IMP] Delivery Orders for Sale Orders with no value are now created with an Invoice Status of 'none'.

--- sale_zero_value_workflow/sale.py
+++ sale_zero_value_workflow/sale.py
@@ -5,11 +5,18 @@ class SaleOrder(osv.Model):
     _name    = 'sale.order'
     _inherit = 'sale.order'
 
-    def test_no_value(self, cr, uid, ids, *args):
+    def test_no_value(self, cr, uid, ids, context=None, *args):
+        ids = isinstance(ids, list) and ids or[ids]
         sale_line_obj = self.pool.get('sale.order.line')
-        line_ids = sale_line_obj.search(cr, uid, [('order_id','in',ids)])
-        line_values = sale_line_obj.read(cr, uid, line_ids, ['price_unit'])
+        line_ids = sale_line_obj.search(cr, uid, [('order_id','in',ids)], context=context)
+        line_values = sale_line_obj.read(cr, uid, line_ids, ['price_unit'], context=context)
         for lv in line_values:
             if lv['price_unit']:
                 return False
         return True
+
+    def _prepare_order_picking(self, cr, uid, order, context=None):
+        res = super(SaleOrder, self)._prepare_order_picking(cr, uid, order, context=context)
+        if self.test_no_value(cr, uid, order.id, context=context):
+            res.update({'invoice_state' : 'none'})
+        return res
