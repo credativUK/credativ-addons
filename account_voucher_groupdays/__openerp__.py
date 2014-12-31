589572f8d206101d00242f9473b1e52520495724
Craig Gowing <craig.gowing@credativ.co.uk>
2014-12-31 11:31:22 +0000

    [IMP] New module to allow account vouchers to be grouped by days

---
+++ account_voucher_groupdays/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution   
+#    Copyright (C) 2014 credativ ltd (<http://www.credativ.co.uk>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Account Voucher Group By Days',
+    'version': '1.0',
+    'category': 'Accounting & Finance',
+    'description':
+        """
+        Allows grouping by days and weeks as well as the standard months
+        """,
+    'author': 'credativ Ltd',
+    'website' : 'http://credativ.co.uk',
+    'depends': ['account_voucher',],
+    'init_xml': [],
+    'update_xml': ['account_view.xml',],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ account_voucher_groupdays/account.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution   
+#    Copyright (C) 2014 credativ ltd (<http://www.credativ.co.uk>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from osv import osv, fields
+import datetime
+
+class account_voucher(osv.osv):
+    _inherit = "account.voucher"
+
+    def _get_group_dates(self, cr, uid, ids, field_name, arg, context=None):
+        res = {}
+        for line in self.browse(cr, uid, ids, context=context):
+            date = datetime.datetime.strptime(line.date, '%Y-%m-%d')
+            if field_name == 'date_day':
+                val = date.strftime('%Y-%m-%d')
+            elif field_name == 'date_week':
+                iso = date.isocalendar()
+                year, week = iso[0], iso[1]-1 # Week is off by one between Python datetime function and ISO calendar
+                date_week_start = datetime.datetime.strptime('%s-%s-1' % (year, week), '%Y-%W-%w')
+                val = "Week of %s" % (date_week_start.strftime('%Y-%m-%d'))
+            res[line.id] = val
+        return res
+
+    _columns = {
+        'date_day': fields.function(_get_group_dates, string='Date (Day)', type='char', size=10,
+                                    store={'account.voucher': (lambda self, cr, uid, ids, c=None: ids, [], 20)}),
+        'date_week': fields.function(_get_group_dates, string='Date (Week)', type='char', size=20,
+                                    store={'account.voucher': (lambda self, cr, uid, ids, c=None: ids, [], 20)}),
+        }
+
+account_voucher()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
