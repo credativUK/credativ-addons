336cf48be50f6ddf3ae57cdff385b57d237b1085
Craig Gowing <craig.gowing@credativ.co.uk>
2015-02-11 09:56:10 +0000

    [IMP] New module which prints delivery addresses on invoices

---
+++ account_invoice_report_delivery/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name" : "Delivery address on invoices",
+    'version': '1.0',
+    "category": 'Accounting & Finance',
+    'description': """
+    Shows delivery addresses of linked sale orders on invoice reports
+    """,
+    'author': 'credativ',
+    'website' : 'http://credativ.co.uk',
+    'depends': ['account',
+                    ],
+    'update_xml': ['account_print_invoice.xml',
+                   ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
