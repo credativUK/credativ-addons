dfa109b4416472e0bc20cea8c6f6c438857245ad
Jacob Hicks <jacob.hicks@credativ.co.uk>
2015-03-17 14:07:18 +0000

    [ADD] Module to allow searching of parent partner tag names

---
+++ res_partner_tag_search/__openerp__.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution   
+#    Copyright (C) 2015 credativ ltd (<http://www.credativ.co.uk>). All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Searching parent partner tags',
+    'version':'1.0',
+    'category' : 'Sales Management',
+    'description': """
+This module allows searching of partner tags by name as well as parent name.
+For example, searching "Asia" would return partners with "Asia" as a tag, as well as "Asia / China" as a tag.
+    """,
+    'author' : 'credativ',
+    'website' : 'http://www.credativ.co.uk',
+    'depends':[
+               'base',
+               ],
+    'update_xml' : [
+        'res_partner_view.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
