995c373beb9371d0f4647a3136531540df8a16bc
Ondřej Kuzník <ondrej.kuznik@credativ.co.uk>
2015-03-25 11:40:27 +0000

    [ADD] mode unit tests to catch issues from testing

--- mrp_minimum_qty/tests/test_order_processing.py
+++ mrp_minimum_qty/tests/test_order_processing.py
@@ -30,6 +30,8 @@ class TestMOMerging(TestStockCommon):
         self.ManufaturingObj = self.env['mrp.production']
         self.RouteObj = self.env['stock.location.route']
         self.BoMObj = self.env['mrp.bom']
+        self.BoMLineObj = self.env['mrp.bom.line']
+        self.Wizard = self.env['mrp.product.produce']
 
         self.manufacturing_route = self.env.ref('mrp.route_warehouse0_manufacture')
         self.manufacturing_rule = self.manufacturing_route.pull_ids[0]
@@ -38,6 +40,13 @@ class TestMOMerging(TestStockCommon):
             'name': 'BoM with minimum Qty',
             'product_tmpl_id': self.productA.product_tmpl_id.id,
             'minimum_qty': 15,
+        })
+
+        self.uom_minimum_line = self.BoMLineObj.create({
+            'bom_id': self.uom_minimum.id,
+            'product_id': self.productB.id,
+            'product_qty': 1,
+            'product_uom': self.productB.uom_id.id,
         })
 
     def test_00_minimum_manuf_qty_satisfied(self):
@@ -95,9 +104,53 @@ class TestMOMerging(TestStockCommon):
             'state': 'confirmed',
         })
 
+        inventory = self.InvObj.create({'name': 'Test',
+                                        'product_id': self.productA.id,
+                                        'filter': 'product'})
+        inventory.prepare_inventory()
+        self.assertFalse(inventory.line_ids, "Inventory line should not created.")
+        inventory_line = self.InvLineObj.create({
+            'inventory_id': inventory.id,
+            'product_id': self.productB.id,
+            'product_uom_id': self.productB.uom_id.id,
+            'product_qty': 17,
+            'location_id': self.stock_location})
+        inventory.action_done()
+        self.assertEqual(self.productB.qty_available, 17, 'Expecting 17 Units , got %.4f Units of quantity available!' % (self.productA.qty_available))
+
         self.ProcurementObj.run_scheduler()
-        planned_mos = self.ManufaturingObj.search([('bom_id','=',self.uom_minimum.id)])
-        self.assertEqual(len(planned_mos), 1, "Wrong number of manufacturing orders created.")
-        self.assertEqual(set(planned_mos[0].procurement_ids), {self.procurement_too_small, self.procurement_dozen}, "Unexpected procurement assigned to MO.")
+        mo = self.ManufaturingObj.search([('bom_id','=',self.uom_minimum.id)])
+        self.assertEqual(len(mo), 1, "Wrong number of manufacturing orders created.")
+
+        self.assertEqual(mo.state, 'ready', "Unexpected MO state.")
+        self.assertEqual(mo.product_qty, 17, "Unexpected MO size.")
+        self.assertEqual(mo.product_uom.id, self.uom_minimum.product_tmpl_id.uom_id.id, "Unexpected MO size.")
+        self.assertEqual(set(mo.procurement_ids), {self.procurement_too_small, self.procurement_dozen}, "Unexpected procurement assigned to MO.")
+
         self.assertEqual(self.procurement_too_small.state, "running", "Unexpected state of the processed procurement")
         self.assertEqual(self.procurement_dozen.state, "running", "Unexpected state of the processed procurement")
+
+        wizard = self.Wizard.with_context({
+            'active_id': mo.id,
+            'active_ids': [mo.id],
+        }).create({
+            'product_qty': 12,
+            'consume_lines': [(0, 0, {
+                'product_id': self.productB.id,
+                'product_qty': 17,
+            })],
+        })
+        wizard.do_produce()
+
+        mo.refresh()
+        self.assertEqual(mo.state, 'in_production', 'Unexpected MO state.')
+
+        wizard = self.Wizard.with_context({
+            'active_id': mo.id,
+            'active_ids': [mo.id],
+        }).create({})
+        self.assertEqual(wizard.product_qty, 5, "Unexpected production size.")
+        wizard.do_produce()
+
+        mo.refresh()
+        self.assertEqual(mo.state, 'done', 'Unexpected MO state.')
