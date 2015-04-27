f662949d8ada3bf5a2bc31999772383af78bd9a4
Craig Gowing <craig.gowing@credativ.co.uk>
2015-04-27 13:48:53 +0100

    [IMP] Apply restrictions to editing orders

--- purchase_order_edit/purchase.py
+++ purchase_order_edit/purchase.py
@@ -48,6 +48,9 @@ class PurchaseOrder(osv.osv, OrderEdit):
         return super(PurchaseOrder, self).copy_data(cr, uid, id_, default, context=context)
 
     def action_picking_create(self, cr, uid, ids, context=None):
+        if self.allocate_check_restrict(cr, uid, ids, context=context):
+            raise osv.except_osv(_('Error!'),
+                _('Purchase order has become restricted and it is no longer possible to edit'))
         line_moves, remain_moves = self.check_consolidation(cr, uid, ids, context)
         res = super(PurchaseOrder, self).action_picking_create(cr, uid, ids, context=context)
         self._fixup_created_picking(cr, uid, ids, line_moves, remain_moves, context)
