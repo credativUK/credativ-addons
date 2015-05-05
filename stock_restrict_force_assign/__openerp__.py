a7920c907c0c92d45ee400f0b672d427e9ee2aae
Craig Gowing <craig.gowing@credativ.co.uk>
2015-05-05 16:55:49 +0100

    [IMP] New module to restrict force assign to stock managers

---
+++ stock_restrict_force_assign/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+        'name' : 'Stock Restrict Force Assign',
+        'version' : '1.0',
+        'author' : 'credativ Ltd',
+        'description' : '''
+Change the force assign buttons on stock move forms to be
+viewable only by stock managers.
+''',
+        'website' : 'http://credativ.co.uk',
+        'depends' : [
+            'stock',
+            ],
+        'init_xml' : [
+            ],
+        'update_xml' : [
+            'stock_view.xml'
+            ],
+        'installable' : True,
+        'active' : False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
