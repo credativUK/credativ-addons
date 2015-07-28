b16856b90ea32e3dc2dfe51aba1390b02a564a28
Ondřej Kuzník <ondrej.kuznik@credativ.co.uk>
2015-07-28 12:34:52 +0100

    [ADD] Label printing

---
+++ stock_picking_barcode_labels/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 credativ Ltd (<http://credativ.co.uk>).
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "Print product labels for a picking",
+    'summary': """Prints product labels for a picking""",
+
+    'author': "credativ ltd",
+    'website': "http://www.credativ.co.uk",
+    'category': 'Stock',
+    'version': '0.1',
+    'depends': ['stock'],
+    'data': [
+        'views/stock_picking_labels.xml',
+        'views/stock_label_wizard.xml',
+    ],
+    'active': False,
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
