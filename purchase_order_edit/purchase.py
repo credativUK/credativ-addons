b800167b629dcbe42c0e6926fdabcc06699e211b
Craig Gowing <craig.gowing@credativ.co.uk>
2016-02-01 18:37:45 +0000

    Revert "[DEBUG] Add profiling"
    This reverts commit b5cfab79cbe10f56c74c29497c831f27a82a7785.

--- purchase_order_edit/purchase.py
+++ purchase_order_edit/purchase.py
@@ -25,10 +25,6 @@ from tools.translate import _
 import netsvc
 from openerp.addons.base_order_edit.order_edit import OrderEdit
 
-import cProfile, pstats, StringIO
-import logging
-_logger = logging.getLogger(__name__)
-
 class PurchaseOrder(osv.osv, OrderEdit):
     _inherit = 'purchase.order'
 
@@ -51,20 +47,9 @@ class PurchaseOrder(osv.osv, OrderEdit):
             return {}
         oe_obj = self.pool.get('purchase.order.edit_wizard')
 
-        pr = cProfile.Profile()
-        pr.enable()
-
         ctx.update({'active_id': ids[0], 'active_ids': [ids[0]]})
         oe_id = oe_obj.create(cr, uid, {}, context=ctx)
-        res = oe_obj.edit_order(cr, uid, [oe_id], context=ctx)
-
-        pr.disable()
-        s = StringIO.StringIO()
-        ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
-        ps.print_stats()
-        _logger.warning('PO Edit IDs %s:\n%s' % (ids, s.getvalue()))
-
-        return res
+        return oe_obj.edit_order(cr, uid, [oe_id], context=context)
 
     def allocate_check_restrict(self, cr, uid, ids, context=None):
         restricted_ids = super(PurchaseOrder, self).allocate_check_restrict(cr, uid, ids, context=context)
@@ -83,10 +68,6 @@ class PurchaseOrder(osv.osv, OrderEdit):
         return super(PurchaseOrder, self).copy_data(cr, uid, id_, default, context=context)
 
     def action_picking_create(self, cr, uid, ids, context=None):
-
-        pr = cProfile.Profile()
-        pr.enable()
-
         for order in self.browse(cr, uid, ids, context=context):
             if order.order_edit_id and self.allocate_check_restrict(cr, uid, [order.order_edit_id.id], context=context):
                 raise osv.except_osv(_('Error!'),
@@ -97,13 +78,6 @@ class PurchaseOrder(osv.osv, OrderEdit):
         for order in self.browse(cr, uid, ids, context=context):
             if order.order_edit_id:
                 self._edit_cancel(cr, uid, [order.order_edit_id.id], 'Edit Cancel:%s' % order.order_edit_id.name, accept_done=True, cancel_assigned=True, context=context)
-
-        pr.disable()
-        s = StringIO.StringIO()
-        ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
-        ps.print_stats()
-        _logger.warning('PO Confirm IDs %s:\n%s' % (ids, s.getvalue()))
-
         return res
 
     def _cancel_check_order(self, purchase, cancel_assigned, accept_done, context=None):
