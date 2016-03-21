1c607de63019e8dd61e354aadc0e7087bbd64cc6
Kinner Vachhani <kinner.vachhani@credativ.co.uk>
2016-03-21 18:20:14 +0000

    [IMP] Code clean up, PEP8, License and convert account_vat.py to new api
    TODO (account_vat_report.py):
    * PEP8
    * Convert to new api
    * Add test cases

--- account_vat_report/models/account_vat.py
+++ account_vat_report/models/account_vat.py
@@ -1,77 +1,62 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2016 credativ Ltd (<http://credativ.co.uk>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2016 credativ Ltd (<http://credativ.co.uk>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import time
+from openerp import models, fields, api, _
 
-from openerp.osv import osv, fields
 
-class account_vat_invoices(osv.TransientModel):
+class account_vat_invoices(models.TransientModel):
     _name = 'account.vat.invoices'
     _description = 'Account Vat Invoices'
-    _columns = {
-        'based_on': fields.selection([('invoices', 'Invoices'),
-                                      ('payments', 'Payments'),],
-                                      'Based on', required=True),
-        'chart_tax_id': fields.many2one('account.tax.code', 'Chart of Tax', help='Select Charts of Taxes', domain = [('parent_id','=', False)]),
-        'display_detail': fields.boolean('Display Detail'),
-        'chart_account_id': fields.many2one('account.account', 'Chart of Account', help='Select Charts of Accounts', domain = [('parent_id','=',False)]),
-        'company_id': fields.related('chart_account_id', 'company_id', type='many2one', relation='res.company', string='Company',),
-        'fiscalyear_id': fields.many2one('account.fiscalyear', 'Fiscal Year', help='Keep empty for all open fiscal year'),
-        'period_from': fields.many2one('account.period', 'Start Period'),
-        'period_to': fields.many2one('account.period', 'End Period'),
-        'date_from': fields.date("Start Date"),
-        'date_to': fields.date("End Date"),
-        'target_move': fields.selection([('posted', 'All Posted Entries'),
-                                         ('all', 'All Entries'),
-                                        ], 'Target Moves'),
-    }
 
-    def _get_tax(self, cr, uid, context=None):
-        taxes = self.pool.get('account.tax.code').search(cr, uid, [('parent_id', '=', False)], limit=1)
+    def _get_tax(self):
+        taxes = self.env['account.tax.code'].search([('parent_id', '=', False)
+                                                     ], limit=1)
         return taxes and taxes[0] or False
 
-    def _get_fiscalyear(self, cr, uid, context=None):
-        now = time.strftime('%Y-%m-%d')
-        fiscalyears = self.pool.get('account.fiscalyear').search(cr, uid, 
-                    [('date_start', '<', now), ('date_stop', '>', now)], limit=1 )
-        return fiscalyears and fiscalyears[0] or False
+    def _get_fiscalyear(self):
+        # Take tz date rather than system date
+        now = fields.Date.context_today(self)
+        return self.env['account.fiscalyear'].find(dt=now, exception=True)
 
-    _defaults = {
-        'based_on': 'invoices',
-        'chart_tax_id': _get_tax,
-        'fiscalyear_id': _get_fiscalyear
-    }
+    based_on = fields.Selection([('invoices', _('Invoices')),
+                                 ('payments', _('Payments')),
+                                 ], default='payments', required=True)
+    chart_tax_id = fields.Many2one(comodel_name='account.tax.code',
+                                   default=_get_tax,
+                                   string=_('Chart of Tax'),
+                                   help=_('Select Charts of Taxes'),
+                                   domain=[('parent_id', '=', False)])
+    display_detail = fields.Boolean(default=True)
+    chart_account_id = fields.Many2one(comodel_name='account.account',
+                                       string=_('Chart of Account'),
+                                       help=_('Select Charts of Accounts'),
+                                       domain=[('parent_id', '=', False)])
+    company_id = fields.Many2one(related=_('chart_tax_id.company_id'))
+    fiscalyear_id = fields.Many2one(comodel_name='account.fiscalyear',
+                                    default=_get_fiscalyear,
+                                    string=_('Fiscal Year'),
+                                    help=_('Keep empty for all open year'))
+    period_from = fields.Many2one(comodel_name='account.period',
+                                  string=_('Start Period'))
+    period_to = fields.Many2one(comodel_name='account.period',
+                                string=_('End Period'))
+    date_from = fields.Date(string=_("Start Date"))
+    date_to = fields.Date(string=_("End Date"))
+    target_move = fields.Selection([('posted', _('All Posted Entries')),
+                                    ('all', _('All Entries')),
+                                    ])
 
-    def create_vat(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-        datas = {'ids': context.get('active_ids', [])}
+    @api.multi
+    def create_vat(self):
+        datas = {'ids': self._context.get('active_ids', [])}
         datas['model'] = 'account.tax.code'
-        datas['form'] = self.read(cr, uid, ids, context=context)[0]
+        datas['form'] = self.read()[0]
         for field in datas['form'].keys():
             if isinstance(datas['form'][field], tuple):
                 datas['form'][field] = datas['form'][field][0]
-        datas['form']['company_id'] = self.pool.get('account.tax.code').browse(cr, uid, [datas['form']['chart_tax_id']], context=context)[0].company_id.id
-
-        return self.pool['report'].get_action(cr, uid, [], 'account_vat_report.vat_invoices', data=datas, context=context)
-
-account_vat_invoices()
+        return self.env['report'].get_action(self,
+                                             'account_vat_report.vat_invoices',
+                                             data=datas)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- account_vat_report/report/account_vat_report.py
+++ account_vat_report/report/account_vat_report.py
@@ -1,30 +1,89 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2016 credativ Ltd (<http://credativ.co.uk>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2016 credativ Ltd (<http://credativ.co.uk>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import time
-from functools import partial
 
 from openerp.osv import osv
 from openerp.report import report_sxw
 from openerp.tools.translate import _
 from openerp.addons.account.report.common_report_header import common_report_header
+
+PAYSQL = "SELECT inv_move_line.tax_amount AS tax_amount,\
+                        inv_move_line.debit AS debit,\
+                        inv_move_line.credit AS credit,\
+                        inv_move_line.date AS date,\
+                        inv_move_line.name AS line_name,\
+                        inv_move_line.ref AS line_ref,\
+                        move.name AS move_name,\
+                        move.id AS move_id,\
+                        account.id AS account_id,\
+                        account.name AS account,\
+                        account.code AS code,\
+                        inv_move_line.tax_code_id,\
+                        invoice.number, \
+                        invoice.date_invoice, \
+                        inv_move_line.credit - inv_move_line.debit as amount, \
+                        invoice.reference, \
+                        partner.name as partner \
+                    FROM \
+                        account_period AS period,\
+                        account_invoice AS invoice,\
+                        account_account AS account,\
+                        res_partner AS partner,\
+                        account_move AS move,\
+                        account_move_line AS line\
+                            JOIN account_move_line AS inv_rec_line\
+                                ON line.reconcile_id = inv_rec_line.reconcile_id\
+                                 AND line.id != inv_rec_line.id\
+                            JOIN account_move_line AS inv_move_line\
+                                ON inv_rec_line.move_id = inv_move_line.move_id\
+                                 AND line.id != inv_move_line.id\
+                            LEFT JOIN account_invoice AS noinv\
+                                ON line.move_id = noinv.move_id\
+                            LEFT JOIN account_move_line AS later_ml\
+                                ON line.reconcile_id = later_ml.reconcile_id\
+                                 AND line.id != later_ml.id\
+                                 AND inv_move_line.id != later_ml.id\
+                                 AND line.date <= later_ml.date\
+                    WHERE line.state <> 'draft'\
+                        AND inv_move_line.tax_code_id = %s\
+                        AND inv_move_line.account_id = account.id\
+                        AND account.company_id = %s\
+                        AND invoice.move_id = inv_move_line.move_id\
+                        AND inv_rec_line.account_id = invoice.account_id\
+                        AND move.id = inv_move_line.move_id\
+                        AND inv_move_line.partner_id = partner.id\
+                        AND line.period_id = period.id\
+                        AND line.period_id IN %s\
+                        AND noinv.id IS NULL\
+                        AND later_ml.id IS NULL\
+                    ORDER BY account.id,account.code,inv_move_line.date, \
+                             move.id, inv_move_line.tax_code_id"
+INVSQL = "SELECT \
+            inv.number, \
+            inv.date_invoice, \
+            SUM(aml.credit - aml.debit) as amount, \
+            act.code, \
+            act.name as account, \
+            inv.reference, \
+            part.name as partner \
+        FROM account_move_line aml \
+        INNER JOIN account_invoice inv \
+            ON aml.move_id = inv.move_id \
+        INNER JOIN account_account act \
+            ON aml.account_id = act.id \
+        LEFT JOIN res_partner part \
+            ON inv.partner_id = part.id \
+        WHERE (aml.credit > 0 OR aml.debit > 0) \
+            AND aml.tax_code_id = %s \
+            AND act.company_id = %s \
+            AND aml.state != 'draft' \
+            AND aml.period_id IN %s \
+        GROUP BY aml.move_id,inv.number,aml.date,inv.date_invoice,act.code, \
+                 act.name,inv.reference,part.name \
+        ORDER BY aml.date"
+
 
 class vat_report(report_sxw.rml_parse, common_report_header):
 
@@ -39,7 +98,7 @@ class vat_report(report_sxw.rml_parse, common_report_header):
             self.period_ids = period_obj.build_ctx_periods(self.cr, self.uid, data['form']['period_from'], data['form']['period_to'])
         else:
             if not self.fiscalyear_id:
-                self.cr.execute ("select id from account_fiscalyear")
+                self.cr.execute("select id from account_fiscalyear")
                 self.fiscalyear_id = self.cr.fetchall()[0][0]
             self.period_ids = period_obj.search(self.cr, self.uid, [('fiscalyear_id', '=', self.fiscalyear_id)])
         return super(vat_report, self).set_context(objects, data, new_ids, report_type=report_type)
@@ -61,7 +120,6 @@ class vat_report(report_sxw.rml_parse, common_report_header):
             'get_tax_code_name': self._get_tax_code_name,
         })
 
-
     def _get_basedon(self, form):
         based_on = form['form']['based_on']
         if based_on == 'invoices':
@@ -69,12 +127,10 @@ class vat_report(report_sxw.rml_parse, common_report_header):
         elif based_on == 'payments':
             return _('Payments')
 
-
     def _get_account(self, data):
         if data.get('form', False) and data['form'].get('chart_account_id', False):
             return self.pool.get('account.account').browse(self.cr, self.uid, data['form']['chart_account_id']).name
         return ''
-
 
     def _get_lines(self, based_on, company_id=False, parent=False, level=0, context=None):
         period_list = self.period_ids
@@ -86,7 +142,7 @@ class vat_report(report_sxw.rml_parse, common_report_header):
         while i < len(res):
 
             res_dict = {
-                'id' : res[i][1].id,
+                'id': res[i][1].id,
                 'code': res[i][1].code,
                 'name': res[i][1].name,
                 'debit': 0,
@@ -98,17 +154,9 @@ class vat_report(report_sxw.rml_parse, common_report_header):
             }
 
             top_result.append(res_dict)
-            res_general = self._get_general(res[i][1].id, period_list, company_id, based_on, context=context)
-            ind_general = 0
-            while ind_general < len(res_general):
-                res_general[ind_general]['type'] = 2
-                res_general[ind_general]['pos'] = 0
-                res_general[ind_general]['level'] = res_dict['level']
-                top_result.append(res_general[ind_general])
-                ind_general+=1
             i+=1
-        return top_result
 
+        return top_result
 
     def _get_general(self, tax_code_id, period_list, company_id, based_on, context=None):
         if not self.display_detail:
@@ -117,29 +165,46 @@ class vat_report(report_sxw.rml_parse, common_report_header):
         obj_account = self.pool.get('account.account')
         periods_ids = tuple(period_list)
         if based_on == 'payments':
-            self.cr.execute('SELECT SUM(line.tax_amount) AS tax_amount, \
-                        SUM(line.debit) AS debit, \
-                        SUM(line.credit) AS credit, \
-                        COUNT(*) AS count, \
-                        account.id AS account_id, \
-                        account.name AS name,  \
-                        account.code AS code \
-                    FROM account_move_line AS line, \
+            self.cr.execute('SELECT sum(inv_move_line.tax_amount) AS tax_amount, \
+                        sum(inv_move_line.debit) AS debit, \
+                        sum(inv_move_line.credit) AS credit, \
+                        count(*) as count, \
+                        inv_move_line.tax_code_id as id \
+                    FROM \
+                        account_period AS period, \
+                        account_invoice AS invoice, \
                         account_account AS account, \
-                        account_move AS move \
-                        LEFT JOIN account_invoice invoice ON \
-                            (invoice.move_id = move.id) \
-                    WHERE line.state<>%s \
-                        AND line.tax_code_id = %s  \
-                        AND line.account_id = account.id \
+                        res_partner AS partner, \
+                        account_move AS move, \
+                        account_move_line AS line \
+                            JOIN account_move_line AS inv_rec_line \
+                                ON line.reconcile_id = inv_rec_line.reconcile_id \
+                                 AND line.id != inv_rec_line.id \
+                            JOIN account_move_line AS inv_move_line \
+                                ON inv_rec_line.move_id = inv_move_line.move_id \
+                                 AND line.id != inv_move_line.id \
+                            LEFT JOIN account_invoice AS noinv \
+                                ON line.move_id = noinv.move_id \
+                            LEFT JOIN account_move_line AS later_ml \
+                                ON line.reconcile_id = later_ml.reconcile_id \
+                                 AND line.id != later_ml.id \
+                                 AND inv_move_line.id != later_ml.id \
+                                 AND line.date <= later_ml.date \
+                    WHERE line.state <> %s \
+                        AND inv_move_line.tax_code_id = %s \
+                        AND inv_move_line.account_id = account.id \
                         AND account.company_id = %s \
-                        AND move.id = line.move_id \
+                        AND invoice.move_id = inv_move_line.move_id \
+                        AND inv_rec_line.account_id = invoice.account_id \
+                        AND move.id = inv_move_line.move_id \
+                        AND inv_move_line.partner_id = partner.id \
+                        AND line.period_id = period.id \
                         AND line.period_id IN %s \
-                        AND ((invoice.state = %s) \
-                            OR (invoice.id IS NULL))  \
-                    GROUP BY account.id,account.name,account.code', ('draft', tax_code_id,
-                        company_id, periods_ids, 'paid',))
-
+                        AND noinv.id IS NULL \
+                        AND later_ml.id IS NULL \
+                    GROUP BY account.id, account.name, account.code, \
+                             inv_move_line.tax_code_id',
+                            ('draft', tax_code_id, company_id, periods_ids, ))
         else:
             self.cr.execute('SELECT SUM(line.tax_amount) AS tax_amount, \
                         SUM(line.debit) AS debit, \
@@ -156,16 +221,11 @@ class vat_report(report_sxw.rml_parse, common_report_header):
                         AND account.company_id = %s \
                         AND line.period_id IN %s\
                         AND account.active \
-                    GROUP BY account.id,account.name,account.code', ('draft', tax_code_id,
-                        company_id, periods_ids,))
+                    GROUP BY account.id,account.name,account.code',
+                            ('draft', tax_code_id, company_id, periods_ids,))
         res = self.cr.dictfetchall()
 
-        i = 0
-        while i<len(res):
-            res[i]['account'] = obj_account.browse(self.cr, self.uid, res[i]['account_id'], context=context)
-            i+=1
         return res
-
 
     def _get_codes(self, based_on, company_id, parent=False, level=0, period_list=[], context=None):
         obj_tc = self.pool.get('account.tax.code')
@@ -193,85 +253,44 @@ class vat_report(report_sxw.rml_parse, common_report_header):
             res.append((account[0], code))
         return res
 
-
     def _get_currency(self, form, context=None):
         return self.pool.get('res.company').browse(self.cr, self.uid, form['company_id'], context=context).currency_id.name
 
+    def _get_account_move_lines(self, tax_id, based_on, company_id=False,
+                                context=None):
 
-    def _get_account_move_lines(self, tax_id, based_on, company_id=False, context=None):
-        cr  = self.cr
-        uid = self.uid
-        date_from = date_to = False
-
-        prd_obj = self.pool.get('account.period')
-
-        fields = [
-                     'inv.number',
-                     'inv.date_invoice',
-                     'SUM(aml.credit - aml.debit)',
-                     'act.code',
-                     'act.name',
-                     'inv.reference',
-                     'part.name as partner',
-                 ]
-
-        period_lst = str(self.period_ids).replace('[','(').replace(']',')')
-
-        sql = 'SELECT ' + ','.join(fields) + ' ' \
-            + 'FROM account_move_line aml ' \
-            + 'INNER JOIN account_invoice inv ' \
-            + 'ON aml.move_id = inv.move_id ' \
-            + 'INNER JOIN account_account act ' \
-            + 'ON aml.account_id = act.id ' \
-            + 'LEFT JOIN res_partner part ' \
-            + 'ON inv.partner_id = part.id ' \
-            + 'WHERE (aml.credit > 0 OR aml.debit > 0) ' \
-            + 'AND aml.state != \'draft\' ' \
-            + 'AND aml.period_id IN %s ' % period_lst
-
-        if company_id:
-            sql += 'AND act.company_id = %d ' % company_id
-
-        if based_on == 'payments':
-            sql += 'AND (inv.state = \'paid\' ' \
-                 + 'OR inv.id IS NULL) '
-
-        sql += 'AND aml.tax_code_id = %s ' % tax_id
-        sql += 'GROUP BY aml.move_id,inv.number,aml.date,inv.date_invoice,act.code,act.name,inv.reference,part.name '
-        sql += 'ORDER BY aml.date'
-
-        cr.execute(sql)
-        result = cr.fetchall()
-
+        cr = self.cr
         tax_code_total = 0.0
-
         ret = []
+
+        sql = based_on == 'payments' and PAYSQL or INVSQL
+        cr.execute(sql, [tax_id, company_id, tuple(self.period_ids)])
+        result = cr.dictfetchall()
+
         for res in result:
-            assert len(res) == len(fields)
-            partner = res[6]
-            #[Fix] Partner name in capital distort texts on report
-            if isinstance(res[6],unicode) and res[6].isupper():
-                partner = str(res[6]).capitalize()
+            partner = res['partner']
+            # [Fix] Partner name in capital distort texts on report
+            if isinstance(partner, unicode) and partner.isupper():
+                partner = str(partner).capitalize()
 
             res_dict = {
-                    'invoice'   : res[0],
-                    'date'      : res[1],
-                    'amount'    : '%0.2f' % res[2],
-                    'act_code'  : res[3],
-                    'account'   : res[4],
-                    'reference' : res[5],
-                    'partner'   : partner[:23],
+                    'invoice': res['number'],
+                    'date': res['date_invoice'],
+                    'amount': '%0.2f' % res['amount'],
+                    'act_code': res['code'],
+                    'account': res['account'],
+                    'reference': res['reference'],
+                    'partner': partner[:23],
             }
             ret.append(res_dict)
-            tax_code_total += float(res[2])
+            tax_code_total += float(res['amount'])
 
         if not self.localcontext.get('tax_code_totals'):
-            self.localcontext.update({'tax_code_totals' : {}})
+            self.localcontext.update({'tax_code_totals': {}})
 
-        self.localcontext['tax_code_totals'].update({tax_id : tax_code_total})
+        self.localcontext['tax_code_totals'].update({tax_id: tax_code_total})
 
         return ret
-
 
     def _get_tax_code_total(self, tax_id, context=None):
         totals = self.localcontext.get('tax_code_totals')
@@ -280,12 +299,12 @@ class vat_report(report_sxw.rml_parse, common_report_header):
             total = totals[tax_id]
         return '%0.2f' % total
 
-
     def _get_tax_code_name(self, tax_id, context=None):
-        cr  = self.cr
+        cr = self.cr
         uid = self.uid
         tax_obj = self.pool.get('account.tax.code')
         return tax_obj.browse(cr, uid, tax_id, context=context).name
+
 
 class report_vat(osv.AbstractModel):
     _name = 'report.account_vat_report.vat_invoices'
@@ -294,4 +313,3 @@ class report_vat(osv.AbstractModel):
     _wrapped_report_class = vat_report
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
- 
